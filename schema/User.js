07d8fb79cf4474c565d6d4da160745ad8a65b2e0
Onuora Ebube Ebuka <onuoraebube44@gmail.com>
2024-06-15 14:51:50 +0100

    Implemented innovative feature, pushing boundaries at the hackathon

--- schema/User.js
+++ schema/User.js
@@ -1,7 +1,7 @@
 
 
 
-import mongoose  from "mongoose";
+import mongoose , { Schema } from "mongoose";
 
 let profile_imgs_name_list = ["Garfield", "Tinkerbell", "Annie", "Loki", "Cleo", "Angel", "Bob", "Mia", "Coco", "Gracie", "Bear", "Bella", "Abby", "Harley", "Cali", "Leo", "Luna", "Jack", "Felix", "Kiki"];
 let profile_imgs_collections_list = ["notionists-neutral", "adventurer-neutral", "fun-emoji"];
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import "dotenv/config"
 import cors from 'cors';
 import bcrypt from 'bcrypt'
 import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken"
 
 //schema***************************************
 import Rating from "./schema/Rating.js";
@@ -59,8 +60,11 @@ server.get('/ratings', async (req, res) => {
 });
 
 //data to send to the frontend
-const formatDatatoSend =() =>{
+const formatDatatoSend =(user) =>{
+
+  const access_token = jwt.sign({ id: user._id}, process.env.SECRET_ACCESS_KEY  )
     return {
+      access_token,
       profile_img : user.personal_info.profile_img,
       username: user.personal_info.username,
       fullname: user.personal_info.fullname
@@ -113,6 +117,37 @@ server.post("/signup" , (req, res) =>{
     })
   })
 
+})
+
+//Route to signin
+
+server.post("/signin", (req,res) =>{
+  let {email, password} = req.body;
+
+  User.findOne({"personal_info.email" : email })
+  .then((user) =>{
+    if(!user){
+      return res.status(403).json({"error": "Email not found"})
+    }
+
+    bcrypt.compare(password, user.personal_info.password, (err, result) =>{
+      if(err){
+        return res.status(403).json({"error": "Error occurred during login, pls try again."})
+      }
+
+      if(!result){
+          return res.status(403).json({"error": "Incorrect Password"})
+      }else{
+        return res.status(200).json(formatDatatoSend(user))
+      }
+    })
+
+    // return res.json({"status" : "user  document"})
+  })
+  .catch(err =>{
+    console.log(err)
+    return res.status(500).json({"error": err.message})
+  })
 })
 
 
